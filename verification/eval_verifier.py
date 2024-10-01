40de73e9f98549d22da6539d8cfa32e32e4a3db6
Sakuna Harinda Jayasundara Hamanheriya Lekamalage <sjay950@foscsmlprd02.its.auckland.ac.nz>
2024-10-01 14:07:34 +1300

    Updated evaluation

--- verification/eval_verifier.py
+++ verification/eval_verifier.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 import warnings
 
@@ -14,16 +14,16 @@ from transformers import (
 )
 from sklearn.metrics import classification_report, matthews_corrcoef
 import pandas as pd
-from dataset.utils import prepare_inputs_bart, create_out_string
+from dataset.utils import prepare_inputs_bart, create_out_string, process_label
 from tqdm import tqdm
 
 
-ver_test_df = pd.read_csv("../data/verification/test_predictions_verification.csv")
+ver_test_df = pd.read_csv("../data/verification/test_verification_dataset.csv")
 
 ID2AUGS = {
     0: "allow_deny",
     1: "csub",
-    #  2: 'cact', // Remove the category since no errors for cact was generated or available in the ground truths of the test set
+    2: 'cact', 
     3: "cres",
     4: "ccond",
     5: "cpur",
@@ -47,16 +47,14 @@ def covert_classes(c):
     return AUGS2ID[c]
 
 
-ver_test_df["label"] = ver_test_df["label"].apply(covert_classes)
-
 truth = ver_test_df["label"].to_list()
 truth_bin = []
 
 for i in truth:
     if i == 11:
-        truth_bin.append(1)
-    else:
         truth_bin.append(0)
+    else:
+        truth_bin.append(1)
 
 
 preds = []
@@ -71,8 +69,7 @@ ver_model.eval()
 for i in tqdm(range(len(ver_test_df))):
 
     input = ver_test_df.iloc[i].input
-    pred_pol = ast.literal_eval(str(ver_test_df.iloc[i].pred))
-    pp = create_out_string(pred_pol)
+    pp = ver_test_df.iloc[i].policies
     ver_inp = prepare_inputs_bart(input, pp, ver_tokenizer, ver_device)
     pred = ver_model(**ver_inp).logits
 
@@ -85,13 +82,13 @@ preds_bin = []
 
 for i in preds:
     if i == 11:
-        preds_bin.append(1)
-    else:
         preds_bin.append(0)
+    else:
+        preds_bin.append(1)
 
 
 print(
-    classification_report(truth_bin, preds_bin, target_names=["incorrect", "correct"])
+    classification_report(truth_bin, preds_bin, target_names=["negative", "positive"])
 )
 print()
 print(f"MCC: {matthews_corrcoef(np.array(truth_bin), np.array(preds_bin))}")
--- verification/train_verifier.py
+++ verification/train_verifier.py
@@ -23,6 +23,9 @@ def train_verifier(id2augs, ds_path='../datasets/verification/combined_verificat
 
     DF = pd.read_csv(ds_path)
     TRAIN_DF, VAL_DF = train_test_split(DF, test_size=0.2, random_state=42)
+    VAL_DF, TEST_DF = train_test_split(VAL_DF, test_size=0.5, random_state=42)
+    
+    TEST_DF.to_csv('../data/verification/test_verification_dataset.csv')
 
     model = AutoModelForSequenceClassification.from_pretrained('facebook/bart-large', num_labels = len(id2augs))
     tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large')
@@ -30,6 +33,7 @@ def train_verifier(id2augs, ds_path='../datasets/verification/combined_verificat
         
     train_ds = VerificationDataset(TRAIN_DF, tokenizer)
     val_ds = VerificationDataset(VAL_DF, tokenizer)
+    test_ds = VerificationDataset(TEST_DF, tokenizer)
     
     path = Path(ckpt_dir)
     path.mkdir(parents=True, exist_ok=True)
@@ -47,7 +51,11 @@ def train_verifier(id2augs, ds_path='../datasets/verification/combined_verificat
         save_strategy="steps",
         save_steps=100,
         logging_steps=100,
-        report_to='none'
+        report_to='none',
+        load_best_model_at_end=True,
+        save_total_limit=1,
+        metric_for_best_model='eval_f1-macro',
+        greater_is_better=True
     )
     
     trainer = Trainer(
@@ -61,16 +69,20 @@ def train_verifier(id2augs, ds_path='../datasets/verification/combined_verificat
 
     trainer.train()
     
+    testing_results = trainer.evaluate(test_ds, metric_key_prefix='test')
+    print(testing_results)
+    
 
 @click.command()
 @click.option('--dataset_path', 
               help='Location of the generated verification dataset',
               show_default=True,
+              default='../data/verification/verification_dataset.csv',
               required=True)
 @click.option('--train_epochs', default=10, help='Number of epochs to train',show_default=True)
 @click.option('--learning_rate', default=2e-5, help='Learning rate',show_default=True)
 @click.option('--batch_size', default=8, help='Batch size',show_default=True)
-@click.option('--out_dir', default='../checkpoints/verification/train', help='Output directory',show_default=True)
+@click.option('--out_dir', default='../checkpoints/verification/', help='Output directory',show_default=True)
 def main(dataset_path, batch_size = 16, learning_rate = 2e-5, train_epochs = 10, 
                    out_dir = 'checkpoints/bart'):
     
@@ -93,6 +105,9 @@ def main(dataset_path, batch_size = 16, learning_rate = 2e-5, train_epochs = 10,
     print(f'Dataset: {dataset_path}\nNum. of classes: {len(ID2AUGS)}\nNum. of epochs: {train_epochs}\nLearning rate: {learning_rate}\nBatch size: {batch_size}\nCheckpoint dir.: {out_dir}\n')
     print(' ======================================================================= \n')
 
+    path = Path(out_dir)
+    path.mkdir(parents=True, exist_ok=True)
+    
     train_verifier(ID2AUGS, ds_path=dataset_path, batch_size = batch_size, learning_rate = learning_rate, train_epochs=train_epochs, ckpt_dir = out_dir)
     
 
